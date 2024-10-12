116ecee6c6653503bf574463851ecb2ab2b0805c
niuiic <niuiicde@gmail.com>
2024-10-12 13:37:36 +0800

    fix: doc, test

--- test.lua
+++ test.lua
@@ -1,5 +1,3 @@
-package.path = package.path .. ";./lua/?.lua;./lua/?/init.lua"
-
 for file_name in io.popen("ls ./lua/omega"):lines() do
 	if string.find(file_name, ".test.", 1, true) then
 		dofile("./lua/omega/" .. file_name)
