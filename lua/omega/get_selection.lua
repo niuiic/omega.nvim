b276dbe68f273a751af2a0487e90f36c300bea65
niuiic <niuiicde@gmail.com>
2024-10-12 22:17:31 +0800

    feat: get_selection work in C-v mode

--- lua/omega/get_selection.lua
+++ lua/omega/get_selection.lua
@@ -7,19 +7,7 @@ function M.get_selection()
 		return vim.fn.expand("cword")
 	end
 
-	local area = require("omega.get_selected_area").get_selected_area()
-
-	if mode == "v" or mode == "V" then
-		local lines = vim.api.nvim_buf_get_lines(0, area.start_lnum - 1, area.end_lnum, false)
-		lines[1] = string.sub(lines[1], area.start_col - 1)
-		lines[#lines] = string.sub(lines[#lines], 1, area.end_col)
-
-		local selection = ""
-		for _, line in ipairs(lines) do
-			selection = selection .. "\n" .. line
-		end
-		return string.sub(selection, 2)
-	end
+	return vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."), { type = mode })
 end
 
 return M
