73cd2e0e8b716c3edf4140113e3fef296c12d5aa
niuiic <niuiicde@gmail.com>
2024-10-16 23:21:02 +0800

    fix: dofile

--- lua/omega/dofile.lua
+++ lua/omega/dofile.lua
@@ -1,17 +1,26 @@
 local M = {}
 
 function M.dofile(file_path)
-	M._reload_deps(file_path)
+	M._record_deps()
 	dofile(file_path)
+	M._reset_deps()
 end
 
-function M._reload_deps(file_path)
-	local lines = vim.fn.readfile(file_path, "")
-	for _, line in ipairs(lines) do
-		for dep in string.gmatch(line, [[require%("(.*)"%)]]) do
-			package.loaded[dep] = nil
-		end
+function M._record_deps()
+	for key, value in pairs(package.loaded) do
+		M._deps[key] = value
 	end
 end
+
+function M._reset_deps()
+	for key, _ in pairs(package.loaded) do
+		if not M._deps[key] then
+			package.loaded[key] = nil
+		end
+	end
+	M._deps = {}
+end
+
+M._deps = {}
 
 return M
