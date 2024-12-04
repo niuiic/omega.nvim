58f33dd34d88db13d2fc4778d51d68c936e663ba
niuiic <niuiicde@gmail.com>
2024-12-04 22:44:44 +0800

    feat(diff_text): remove vim.schedule

--- lua/omega/diff_text.lua
+++ lua/omega/diff_text.lua
@@ -4,9 +4,7 @@ function M.diff_text(old_text, new_text, callback)
 	require("omega-rs.cli").call({ "diff", "--old-text", old_text, "--new-text", new_text }, nil, function(result)
 		if result.code == 0 then
 			local text_edits = vim.json.decode(result.stdout)
-			vim.schedule(function()
-				callback(text_edits)
-			end)
+			callback(text_edits)
 		end
 	end)
 end
