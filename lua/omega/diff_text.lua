c9a14a10310ad2e4c241b52fc4f3d1ba49ef02e5
niuiic <niuiicde@gmail.com>
2024-12-23 18:40:07 +0800

    fix: diff_text

--- lua/omega/diff_text.lua
+++ lua/omega/diff_text.lua
@@ -4,7 +4,7 @@ function M.diff_text(old_text, new_text, callback)
 	old_text = string.format('"%s"', old_text)
 	new_text = string.format('"%s"', new_text)
 	require("omega-rs.cli").call({ "diff", "--old-text", old_text, "--new-text", new_text }, nil, function(result)
-		if result.code == 0 then
+		if result.code == 0 and result.stdout ~= "" then
 			local text_edits = vim.json.decode(result.stdout)
 			callback(text_edits)
 		end
