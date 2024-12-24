138b707635ef5fd0c46ca75e047bb94d56d41aaf
niuiic <niuiicde@gmail.com>
2024-12-24 09:36:29 +0800

    fix(diff_text): long text

--- lua/omega/diff_text.lua
+++ lua/omega/diff_text.lua
@@ -1,14 +1,49 @@
 local M = {}
 
 function M.diff_text(old_text, new_text, callback)
-	old_text = string.format('"%s"', old_text)
-	new_text = string.format('"%s"', new_text)
-	require("omega-rs.cli").call({ "diff", "--old-text", old_text, "--new-text", new_text }, nil, function(result)
-		if result.code == 0 and result.stdout ~= "" then
-			local text_edits = vim.json.decode(result.stdout)
-			callback(text_edits)
+	xpcall(
+		require("omega-rs.cli").call,
+		function()
+			callback(M._get_full_text_edits(old_text, new_text))
+		end,
+		{ "diff", "--old-text", string.format('"%s"', old_text), "--new-text", string.format('"%s"', new_text) },
+		nil,
+		function(result)
+			if result.code == 0 and result.stdout ~= "" then
+				local ok, text_edits = pcall(vim.json.decode, result.stdout)
+				if not ok then
+					text_edits = M._get_full_text_edits(old_text, new_text)
+				end
+
+				callback(text_edits)
+			else
+				callback({})
+			end
 		end
-	end)
+	)
+end
+
+function M._get_full_text_edits(old_text, new_text)
+	local old_lines = vim.fn.split(old_text, require("omega").get_line_ending(0))
+	local last_old_line_chars = require("omega").get_chars(old_lines[#old_lines])
+
+	local text_edits = {
+		{
+			range = {
+				start = {
+					line = 0,
+					character = 0,
+				},
+				["end"] = {
+					line = #old_lines - 1,
+					character = #last_old_line_chars,
+				},
+			},
+			newText = new_text,
+		},
+	}
+
+	return text_edits
 end
 
 return M
