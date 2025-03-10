8db2e479765cd4f954c528286718eb4c124cc10a
niuiic <niuiicde@gmail.com>
2025-03-10 22:31:29 +0800

    test: async

--- lua/omega/async.test.lua
+++ lua/omega/async.test.lua
@@ -3,11 +3,12 @@ local await = require("omega.await").await
 
 local function async_fn(callback)
 	vim.defer_fn(function()
-		callback(100)
+		callback(100, true)
 	end, 100)
 end
 
 async(function()
-	local result = await(async_fn)
-	assert(result == 100)
+	local data1, data2 = await(async_fn)
+	assert(data1, 100)
+	assert(data2, true)
 end)()
